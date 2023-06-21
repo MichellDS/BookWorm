d8e380dfeb0dd11cc682f6f675d8c3c2f811cb2b
MARCOS SIMIONATO <marcos_codornamh@hotmail.com>
2023-06-21 19:55:19 -0300

    ajuste WindowManager

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:window_manager/window_manager.dart';
 import 'pages/main_page.dart';
+import 'generated/l10n.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
 
-
-void main() {
-  WindowOptions windowOptions = WindowOptions(
+  WindowOptions windowOptions = const WindowOptions(
       windowButtonVisibility: true,
       center: true,
-      size: Size(850, 600),
-      minimumSize: Size(850, 600));
+      size: Size(1024, 768),
+      minimumSize: Size(1024, 768));
   windowManager.waitUntilReadyToShow(windowOptions, () async {
     await windowManager.show();
+    await windowManager.setMaximizable(false);
+    await windowManager.setResizable(false);
     await windowManager.focus();
   });
   runApp(const MyApp());
